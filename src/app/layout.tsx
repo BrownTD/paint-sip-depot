7098b19b7c7dd3314777806fb8cf481663450bb9
Nevyn Brown <nevynjbrown1@gmail.com>
2026-01-24 21:26:48 -0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import { Familjen_Grotesk, Oswald } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
-export const familjenGrotesk = Familjen_Grotesk({
+const familjenGrotesk = Familjen_Grotesk({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-sans",
   display: "swap",
 });
 
-export const oswald = Oswald({
+const oswald = Oswald({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-display",
